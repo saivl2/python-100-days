c1a284c5b3fbc5a11824ea5d01c814c7ae813b25
saivl2 <supriyavadali123@gmail.com>
2025-11-05 15:34:19 +0530

    Added numbers_operators_comments

---
+++ 01-numbers_operators_comments/comments.py
@@ -0,0 +1,6 @@
+"""
+This module demonstrates the use of comments in Python code.
+"""
+print(1/3)
+# This is a single-line comment explaining the following line of code
+print(42)
---
+++ 01-numbers_operators_comments/numbers.py
@@ -0,0 +1,11 @@
+# Integers
+print(type(4))
+print(-90)
+print(57)
+
+# Floating Point Numbers
+print(type(3.14))
+print(-0.001)
+print(2.0)
+print(-1.233333333333333333333333333333) # Python handles precision - it will round the number
+print(4.0 + 1)
