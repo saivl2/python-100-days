14a8e797b14bf01129d07de438dbc735d93c90d0
saivl2 <supriyavadali123@gmail.com>
2025-11-12 23:33:31 +0530

    added day-1

---
+++ day-1/string_manipulation.py
@@ -0,0 +1,4 @@
+print("hello world\nhello world\nhello world")
+
+# Concatenate strings 
+print('Hello '+ 'John')
---
+++ day-1/variables.py
@@ -0,0 +1,8 @@
+name = input("What is your name? ")
+
+# Get the characters
+print(len(name))
+print(name)
+
+name = 'John'
+print(name)
